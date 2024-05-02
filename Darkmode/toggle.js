7c19fbda859bb5bf00a5f2c10891036f5ad8581e
RyanYang99 <diddbstjd0220@naver.com>
2024-05-02 19:41:43 +0900

    다크 모드 기능 성공

---
+++ Darkmode/toggle.js
@@ -0,0 +1,29 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+const buttonText = document.querySelector('.button-text');
+const box = document.getElementById('box');
+const icon = document.querySelector('.icon');
+
+function toggleDarkMode() {
+  body.classList.toggle('dark-mode');
+  box.classList.toggle('dark-mode');
+  darkModeToggle.classList.toggle('toggled');
+  icon.classList.toggle('light-mode');
+  icon.classList.toggle('dark-mode');
+
+  // 클릭시 모드 이름 적용
+if (body.classList.contains('dark-mode')) {
+    buttonText.textContent = 'Light Mode On';
+  } else {
+    buttonText.textContent = 'Dark Mode On';
+  }
+}
+
+
+
+
+// 클릭시 다크모드 혹은 라이트모드로 적용
+darkModeToggle.addEventListener('click', toggleDarkMode);
+
+// 페이지 로드시 초기 라이트모드로 설정
+applyInitialTheme();
