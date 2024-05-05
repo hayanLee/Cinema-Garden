aae5f9dacc6b119fd8d75a96938f30a5407c6ea3
hayanLee <hayanlee.dev@gmail.com>
2024-05-05 22:19:03 +0900

    개봉예정작 티저영상 삽입

--- js/movie.js
+++ js/movie.js
@@ -1,15 +1,16 @@
 //영화 정보를 가져와서 ui에 리스팅
 // const $cardContainer = document.querySelector('.cardContainer');
 const $main_cards = document.querySelector('.main__cards');
+const $videos = document.querySelector('.videos');
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         accept: 'application/json',
-//         Authorization:
-//             'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWNmOGYwMTA4YjcwODA3NDI0YWVhZjFkMDExMGY2NSIsInN1YiI6IjYyNTdiZjFmZDZkYmJhMDA5OGM0MGFlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ee2ocg2kilfxcTv_uW6jbBUliMHyw6f59mBKiBICEx8',
-//     },
-// };
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWNmOGYwMTA4YjcwODA3NDI0YWVhZjFkMDExMGY2NSIsInN1YiI6IjYyNTdiZjFmZDZkYmJhMDA5OGM0MGFlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ee2ocg2kilfxcTv_uW6jbBUliMHyw6f59mBKiBICEx8',
+    },
+};
 
 // 영화 데이터만 가져옴 (API)
 // export async function fetchMovies() {
@@ -28,8 +29,8 @@ async function fetchMovies() {
     const { results } = await response.json();
     return results;
 }
-// 영화(array) 가져와서 html에 뿌려주기
 
+// 영화(array) 가져와서 html에 뿌려주기
 export async function printMovieCard() {
     try {
         const movies = await fetchMovies(); // array(20)
@@ -91,6 +92,46 @@ const createMovieCard = ({ imgSrc, title, content, id, releaseDate }) => {
     $main_cards.appendChild(card);
 
     card.addEventListener('click', clickCard); // 이벤트 버블링
+};
+
+// 개봉예정 영화 영상
+async function getUpcommingVideoIDs() {
+    // const response = await fetch(
+    //     `https://api.themoviedb.org/3/movie/upcoming?language=ko&page=1`,
+    //     options
+    // );
+    const response = await fetch('../assets/upcomming.json');
+    const { results } = await response.json();
+    const videoIDs = results.map((video) => video.id);
+    return videoIDs;
+}
+
+async function getMovieVideos(movieId) {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?language=ko`,
+        options
+    );
+    const { results } = await response.json();
+    const teaser = results.filter(
+        (data) => data.type === 'Teaser' || data.type === 'Trailer'
+    );
+
+    if (teaser.length > 0) {
+        // console.log(teaser[teaser.length - 1]);
+        createVideoCard(teaser[teaser.length - 1].key);
+    }
+}
+
+const videoIDs = getUpcommingVideoIDs(); // 비디오 아이디 배열
+videoIDs.then((arr) => {
+    arr.forEach((movieId) => getMovieVideos(movieId));
+});
+
+const createVideoCard = (videoId) => {
+    const videoCard = document.createElement('iframe');
+    videoCard.src = `https://www.youtube.com/embed/${videoId}`;
+    videoCard.classList.add('videoCard');
+    $videos.appendChild(videoCard);
 };
 
 const clickCard = (e) => {
