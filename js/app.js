f1980043877c8d8dc6e0a9b6e2aa836f25612b81
hayanLee <hayanlee.dev@gmail.com>
2024-05-06 18:20:44 +0900

    메인페이지 스타일링

--- js/app.js
+++ js/app.js
@@ -2,19 +2,38 @@ import { printMovieCard } from './movie.js';
 import { handleForm } from './search.js';
 const $searchForm = document.querySelector('.searchForm');
 const $input = document.querySelector('.searchForm__input');
-const $header = document.querySelector('.header__head > .title');
+const $logo = document.querySelector('.header__head > .title');
+const $header = document.querySelector('.header__head');
+const $uptoBtn = document.querySelector('.uptoBtn');
 // const $uptoBtn = document.querySelector('.uptoBtn');
 
 printMovieCard(); // movie module
 
 // document.addEventListener('DOMContentLoaded', () => $input.focus());
-// $header.addEventListener('click', () => {
-//     window.location.reload();
-// });
-// $uptoBtn.addEventListener('click', () => {
-//     window.scrollTo({
-//         top: 0,
-//         behavior: 'smooth',
-//     });
-// });
+$logo.addEventListener('click', () => {
+    window.location.reload();
+});
+
+$uptoBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
+function scrollEvent(e) {
+    const STANDARD = 600;
+
+    if (window.scrollY > STANDARD) {
+        // 내려갈 때
+        $uptoBtn.classList.add('show');
+        $header.classList.add('bg');
+    } else {
+        // 올라갈 때
+        $uptoBtn.classList.remove('show');
+        $header.classList.remove('bg');
+    }
+}
+
+window.addEventListener('scroll', scrollEvent);
 $searchForm.addEventListener('submit', handleForm);
